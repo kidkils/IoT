aca7467fd60ff9d424818ee484bd8602f2c9c93e
kidkils <66657496+kidkils@users.noreply.github.com>
2021-11-03 10:59:57 +0800

    Update subscribe_mesh.py

--- subscribe_mesh.py
+++ subscribe_mesh.py
@@ -38,3 +38,29 @@ channel.exchange_declare(exchange='test-exchange', exchange_type='topic', durabl
 result=channel.queue_declare(queue='', exclusive=True)
 
 queue_name=result.method.queue
+
+channel.queue_bind(queue=queue_name, exchange='test-exchange', routing_key="mesh-Gateway")
+
+def insert_to_log(data):
+    msg = json.loads(data)
+    namenode = 'node' + str(msg['node'])
+
+    for i in msg:
+        if i != 'node':
+            measurement = i
+            value = msg[i]
+            logging.basicConfig(filename='test.log', filemode='a', format='%(asctime)s -- %(message)s')
+            logging.error(measurement + ' -- ' + namenode + ' -- ' + str(value))
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    insert_to_influx(body)
+    insert_to_log(body)
+
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+channel.start_consuming()
+
